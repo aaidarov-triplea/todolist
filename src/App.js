a3c318177f3144aa47fc778d1e34dc31e13e79b5
aaidarov-triplea <aaidarov.triplea@gmail.com>
2023-03-28 20:46:28 +0600

    add own hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,51 @@
-import { useState } from 'react'
+import styled from 'styled-components'
 import { TodoList } from './components/todo-list'
 import { startTodolist } from './data'
+import { useTodoList } from './hooks/useTodoList'
+
+
+
+const StyledTodoList = styled(TodoList)`
+  background-color: coral;
+  margin-top: 50px;
+
+`
 
 function App() {
-  const [todos, setTodos] = useState(startTodolist)
+
+  const { todos, toggleTodo } = useTodoList(startTodolist)
 
   const getOverdueTodos = () => {
     const today = new Date()
-    return todos.filter((todo) => new Date(todo.deadline) < today)
+    return todos.filter((todo) => !todo.isDone && new Date(todo.deadline) < today)
   }
 
   const getActualTodos = () => {
     const today = new Date()
-    return todos.filter((todo) => new Date(todo.deadline) >= today)
+    return todos.filter((todo) => !todo.isDone && new Date(todo.deadline) >= today)
   }
 
-  const toggleTodo = (id) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isDone: !todo.isDone }
-      } else {
-        return todo
-      }
-    })
-    setTodos(updatedTodos)
-  }
+const getComplateTodos = () => {
+  return todos.filter(todo => todo.isDone)
+}
 
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoList
+      <StyledTodoList
         title="Overdue"
         items={getOverdueTodos()}
         onToggleTodo={toggleTodo}
       />
-      <TodoList
+      <StyledTodoList
         title="Actual"
         items={getActualTodos()}
+        onToggleTodo={toggleTodo}
+      />
+
+      <StyledTodoList
+        title="Complate"
+        items={getComplateTodos()}
         onToggleTodo={toggleTodo}
       />
     </div>
---
+++ src/hooks/useTodoList.js
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+//создаем собственный хук
+ export const useTodoList = (initTodo) => {
+    const [todos, setTodos] = useState(initTodo)
+  
+    const toggleTodo = (id) => {
+      const updatedTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isDone: !todo.isDone }
+        } else {
+          return todo
+        }
+      })
+      setTodos(updatedTodos)
+    }
+  
+    return {
+      todos,
+      toggleTodo,
+    }
+  }
